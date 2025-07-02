c5ad29192f82a71fcea4e36c323175a977a97f9f
studentsharma <146518790+studentsharma@users.noreply.github.com>
2025-07-02 19:09:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ import cors from "cors"
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+const allowedOrigins = ['https://rescueus.onrender.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 
 
 app.use("/help", helping);
