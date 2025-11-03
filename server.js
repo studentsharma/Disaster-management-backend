2709fd9a8a7be86b0d5f24f170b8c334152bd070
studentsharma <146518790+studentsharma@users.noreply.github.com>
2025-11-03 18:02:08 +0530

    Add localhost to allowed CORS origins

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 
-const allowedOrigins = ['https://rescueus.onrender.com'];
+const allowedOrigins = ['http://localhost:5173','https://rescueus.onrender.com'];
 
 app.use(cors({
   origin: function (origin, callback) {
